5df25619c8661566c03e87da9db2779445d8cd2b
aymanshawqi360 <ayman22ahmadan2017@gmail.com>
2025-10-13 16:30:54 +0300

    -added string_manager to util folder

---
+++ lib/core/util/string_manager.dart
@@ -0,0 +1,8 @@
+class StringManager {
+  StringManager._();
+  static const String title = "Welcome to ShopZen";
+  static const String description =
+      "Your one-stop destination for hassle-\nfree online shopping";
+  static const String getStarted = "Get Started";
+  static const String satoshiMedium = "SatoshiMedium";
+}
