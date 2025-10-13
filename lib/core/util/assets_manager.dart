2a6ab270f1c857911e8d33e325bd48e021acf920
aymanshawqi360 <ayman22ahmadan2017@gmail.com>
2025-10-13 16:27:37 +0300

    -added assets_manager to util folder

---
+++ lib/core/util/assets_manager.dart
@@ -0,0 +1,9 @@
+class AssetsManager {
+  static const String __assetsImage = "assets/image/";
+  static const String __assetsSvg = "assets/svg/";
+
+  //!Image
+
+  //!Svg
+  static const String onboarding = '${__assetsSvg}onboarding.svg';
+}
