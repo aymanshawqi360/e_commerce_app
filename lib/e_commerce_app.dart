b446f481bf19638c76e80a3baa140fd67eeae791
aymanshawqi360 <ayman22ahmadan2017@gmail.com>
2025-10-13 16:32:38 +0300

    -added e_commerce_app

---
+++ lib/e_commerce_app.dart
@@ -0,0 +1,27 @@
+import 'package:e_commerce_app/core/routing/app_route.dart';
+import 'package:e_commerce_app/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ECommerceApp extends StatelessWidget {
+  final AppRoute appRoute;
+  const ECommerceApp({super.key, required this.appRoute});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+
+          initialRoute: Routes.onboarding,
+          onGenerateRoute: appRoute.gernrateRoute,
+        );
+      },
+    );
+  }
+}
