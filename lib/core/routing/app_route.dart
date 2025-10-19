08ed71b3d97d9eb2ca3a6bc5c0b62c484c79ad6e
Ayman Shawqi <ayman22ahmadan2017@gmail.com>
2025-10-19 14:49:46 +0300

    -added sign_up route to app_routes

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/core/routing/routes.dart';
 import 'package:e_commerce_app/features/auth/presentation/screen/login_screen.dart';
+import 'package:e_commerce_app/features/auth/presentation/screen/sign_up_screen.dart';
 import 'package:e_commerce_app/features/onboarding/presentation/screen/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,8 @@ class AppRoute {
         return MaterialPageRoute(builder: (_) => OnboardingScreen());
       case Routes.login:
         return MaterialPageRoute(builder: (_) => LoginScreen());
+      case Routes.signUp:
+        return MaterialPageRoute(builder: (_) => SignUpScreen());
 
       default:
         deafultScreen();
