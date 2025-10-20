71c097e06b030d2b455abe1b78da9d4aadff7d07
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-20 17:30:13 +0300

    - deleted main.dart

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:e_commerce_app/core/routing/app_route.dart';
-import 'package:e_commerce_app/e_commerce_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(ECommerceApp(appRoute: AppRoute()));
-}
