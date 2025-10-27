f36c59f6154110eac75681ddd1d011c94c351c80
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-27 17:05:52 +0300

    -update signup_response_mode

--- lib/features/auth/data/model/sign_up/signup_response_model.dart
+++ lib/features/auth/data/model/sign_up/signup_response_model.dart
@@ -6,7 +6,9 @@ class SignupResponseModel {
   int? id;
   String? email;
   String? roles;
-  SignupResponseModel({this.id, this.email, this.roles});
+  @JsonKey(name: 'terms_accepted')
+  bool? termsAccepted;
+  SignupResponseModel({this.id, this.email, this.roles, this.termsAccepted});
   factory SignupResponseModel.fromJson(Map<String, dynamic> json) =>
       _$SignupResponseModelFromJson(json);
 
